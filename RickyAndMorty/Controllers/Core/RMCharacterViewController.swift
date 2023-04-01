4c0fb64833948df8c96dea02af9367627e7841f3
daicl <dcl.outlook.1016@gmail.com>
2023-04-01 21:27:54 +0800

    Create API Request

--- RickyAndMorty/Controllers/Core/RMCharacterViewController.swift
+++ RickyAndMorty/Controllers/Core/RMCharacterViewController.swift
@@ -14,6 +14,23 @@ class RMCharacterViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = "Characters"
+        
+        let request = RMRequest(endpoint: .character,
+                                pathComponents: ["1"],
+                                queryParameters: [
+                                    URLQueryItem(name: "name", value: "rick"),
+                                    URLQueryItem(name: "status", value: "alive")
+                                ])
+        print(request.url)
+        
+        RMService.shared.execute(request, expecting: RMCharacter.self) { result in
+            switch result {
+            case .success(let success):
+                <#code#>
+            case .failure(let failure):
+                <#code#>
+            }
+        }
     }
 
 
