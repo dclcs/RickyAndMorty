93e6b9d1851a005c1727d0a2b3b9ff441261a84f
daicl <dcl.outlook.1016@gmail.com>
2023-04-06 23:29:54 +0800

    Update RMEpisodeInfoCollectionViewCellViewModel.swift

--- RickyAndMorty/ViewModels/EpisodeDetail/RMEpisodeInfoCollectionViewCellViewModel.swift
+++ RickyAndMorty/ViewModels/EpisodeDetail/RMEpisodeInfoCollectionViewCellViewModel.swift
@@ -6,13 +6,7 @@
 //
 
 import Foundation
-final class RMEpisodeInfoCollectionViewCellViewModel {
+struct RMEpisodeInfoCollectionViewCellViewModel {
     public let title: String
     public let value: String
-    
-    
-    init(title: String, value: String) {
-        self.title = title
-        self.value = value
-    }   
 }
