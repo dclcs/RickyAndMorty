321e25766dc40cdb661c2a18f2a1df66595ad99b
daicl <dcl.outlook.1016@gmail.com>
2023-04-06 23:29:36 +0800

    Add: episode detail layout

--- RickyAndMorty/ViewModels/EpisodeDetail/RMEpisodeDetailViewViewModel.swift
+++ RickyAndMorty/ViewModels/EpisodeDetail/RMEpisodeDetailViewViewModel.swift
@@ -13,42 +13,42 @@ protocol RMEpisodeDetailViewViewModelDelegate: AnyObject {
 
 final class RMEpisodeDetailViewViewModel {
     private let endpointUrl: URL?
-    private var dataTuple: (RMEpisode, [RMCharacter])? {
+    private var dataTuple: (episode: RMEpisode, characters: [RMCharacter])? {
         didSet {
+            createCellViewModels()
             delegate?.didFinishEpisodeDetails()
         }
     }
-    
+
     enum SectionType {
         case information(viewModel: [RMEpisodeInfoCollectionViewCellViewModel])
         case characters(viewModel: [RMCharacterCollectionViewCellViewModel])
-        
     }
 
     public weak var delegate: RMEpisodeDetailViewViewModelDelegate?
-    
-    public private(set) var sections: [SectionType] = []
+
+    public private(set) var cellViewModels: [SectionType] = []
 
     // MARK: - Init
-    
+
     init(endpointUrl: URL?) {
         self.endpointUrl = endpointUrl
         fetchEpisodeData()
     }
-    
+
     // MARK: - Public
-    
+
     /// Fetch backing Episode model
     public func fetchEpisodeData() {
         guard let url = endpointUrl,
               let request = RMRequest(url: url) else {
             return
         }
-        
+
         RMService.shared.execute(request, expecting: RMEpisode.self) { [weak self] result in
             guard let self = self else { return }
             switch result {
-            case .success(let model):
+            case let .success(model):
                 print(String(describing: model))
                 self.fetchRelatedCharacters(episode: model)
             case .failure:
@@ -56,18 +56,38 @@ final class RMEpisodeDetailViewViewModel {
             }
         }
     }
-    
+
     // MARK: - Private
+
+    private func createCellViewModels() {
+        guard let dataTuple = dataTuple else { return }
+        let episode = dataTuple.episode
+        let characters = dataTuple.characters
+        cellViewModels = [
+            .information(viewModel: [
+                .init(title: "Episode Name", value: episode.name),
+                .init(title: "Air Date", value: episode.air_date),
+                .init(title: "Episode", value: episode.episode),
+                .init(title: "Created", value: episode.created),
+            ]),
+            .characters(viewModel: characters.compactMap({ character in
+                return RMCharacterCollectionViewCellViewModel(characterName: character.name,
+                                                       characterStatus: character.status,
+                                                       characterImageUrl: URL(string: character.image))
+            })),
+        ]
+    }
+
     private func fetchRelatedCharacters(episode: RMEpisode) {
         let requests = episode.characters.compactMap({
-            return URL(string: $0)
+            URL(string: $0)
         }).compactMap({
-            return RMRequest(url: $0)
+            RMRequest(url: $0)
         })
-        
+
         // 10 of parallel requests
         // Notified once all done
-        
+
         let group = DispatchGroup()
         var characters: [RMCharacter] = []
         for request in requests {
@@ -78,22 +98,19 @@ final class RMEpisodeDetailViewViewModel {
                     group.leave()
                 }
                 switch result {
-                case .success(let model):
+                case let .success(model):
                     characters.append(model)
                 case .failure:
                     break
                 }
             }
         }
-        
+
         group.notify(queue: .main) {
             self.dataTuple = (
                 episode,
                 characters
             )
         }
-        
-        
     }
-    
 }
--- RickyAndMorty/Views/EpisodeDetails/RMEpisodeDetailView.swift
+++ RickyAndMorty/Views/EpisodeDetails/RMEpisodeDetailView.swift
@@ -11,6 +11,7 @@ final class RMEpisodeDetailView: UIView {
     private var viewModel: RMEpisodeDetailViewViewModel? {
         didSet {
             spinner.stopAnimating()
+            self.collectionView?.reloadData()
             self.collectionView?.isHidden = false
             UIView.animate(withDuration: 0.3) {
                 self.collectionView?.alpha = 1
@@ -70,7 +71,8 @@ final class RMEpisodeDetailView: UIView {
         collectionView.alpha = 0
         collectionView.delegate = self
         collectionView.dataSource = self
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.register(RMEpisodeInfoCollectionViewCell.self, forCellWithReuseIdentifier: RMEpisodeInfoCollectionViewCell.cellIdentifier)
+        collectionView.register(RMCharacterCollectionViewCell.self, forCellWithReuseIdentifier: RMCharacterCollectionViewCell.cellIdentifier)
         return collectionView
     }
     
@@ -83,17 +85,42 @@ final class RMEpisodeDetailView: UIView {
 
 extension RMEpisodeDetailView: UICollectionViewDelegate, UICollectionViewDataSource {
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
+        return viewModel?.cellViewModels.count ?? 0
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        guard let sections = viewModel?.cellViewModels else { return 0 }
+        let sectionType = sections[section]
+        switch sectionType {
+        case .information(let viewModel):
+            return viewModel.count
+        case .characters(let viewModel):
+            return viewModel.count
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        cell.backgroundColor = .yellow
-        return cell
+        guard let sections = viewModel?.cellViewModels else {
+            fatalError("No ViewModel")
+        }
+        let sectionType = sections[indexPath.section]
+        switch sectionType {
+        case .information(let viewModel):
+            let cellModel = viewModel[indexPath.row]
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RMEpisodeInfoCollectionViewCell.cellIdentifier, for: indexPath) as? RMEpisodeInfoCollectionViewCell else {
+                fatalError()
+            }
+            cell.configure(with: cellModel)
+            return cell
+            
+        case .characters(let viewModel):
+            let cellModel = viewModel[indexPath.row]
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RMCharacterCollectionViewCell.cellIdentifier, for: indexPath) as? RMCharacterCollectionViewCell else {
+                fatalError()
+            }
+            cell.configure(with: cellModel)
+            return cell
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
@@ -103,14 +130,49 @@ extension RMEpisodeDetailView: UICollectionViewDelegate, UICollectionViewDataSou
 
 extension RMEpisodeDetailView {
     private func layout(for section: Int) -> NSCollectionLayoutSection {
-        let item = NSCollectionLayoutItem(layoutSize: .init(widthDimension: .fractionalWidth(1),
-                                                            heightDimension: .fractionalHeight(1)))
-        item.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
-        let group = NSCollectionLayoutGroup.vertical(layoutSize: .init(
-            widthDimension: .fractionalWidth(1),
-            heightDimension: .absolute(100)),
-                                                     subitems: [item])
         
+        guard let sections = viewModel?.cellViewModels else { return createInfoLayout() }
+        switch sections[section] {
+        case .information:
+            return createInfoLayout()
+        case .characters:
+            return createCharacterLayout()
+        }
+    }
+    
+    func createInfoLayout() -> NSCollectionLayoutSection {
+        let item = NSCollectionLayoutItem(layoutSize: .init(
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .fractionalHeight(1))
+        )
+        
+        item.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+        
+        let group = NSCollectionLayoutGroup.vertical(
+            layoutSize: .init(widthDimension: .fractionalWidth(1),
+                              heightDimension: .absolute(100)),
+            subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        return section
+    }
+    
+    func createCharacterLayout() -> NSCollectionLayoutSection {
+        let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(0.5),
+            heightDimension: .fractionalHeight(1.0)))
+        item.contentInsets = NSDirectionalEdgeInsets(
+            top: 5,
+            leading: 10,
+            bottom: 5,
+            trailing: 10
+        )
+
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .absolute(260)),
+            subitems: [item, item])
         let section = NSCollectionLayoutSection(group: group)
         return section
     }
